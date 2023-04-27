fbce3fa7b7f6c97f570173c2b122b97c70cfaa91
khanhph4 <khanhph4@fpt.com.vn>
2023-04-27 21:03:54 +0700

    add chatgptapi

--- app/libs/chatgptapi.py
+++ app/libs/chatgptapi.py
@@ -4,7 +4,7 @@ from logzero import logger
 from datetime import datetime, timedelta
 
 
-def check_credit_usage(api_token):
+def check_credit_usage_by_date(api_token):
     date_format = '%Y-%m-%d'
     end_date = datetime.utcnow().strftime(date_format)
     start_date = (datetime.utcnow() - timedelta(days=100)
@@ -29,9 +29,31 @@ def check_credit_usage(api_token):
         logger.info(response.text)
 
 
+def get_credit_usage(api_token):
+    url = "https://api.openai.com/dashboard/billing/subscription"
+
+    payload = {}
+    headers = {
+        'Authorization': f'Bearer {api_token}'
+    }
+
+    logger.info(headers)
+    response = requests.request("GET", url, headers=headers, data=payload)
+    logger.info(response.status_code)
+    data = response.json()
+    if response.status_code == 200 and data:
+        soft_limit_usd = data.get('soft_limit_usd')
+        hard_limit_usd = data.get('hard_limit_usd')
+        if soft_limit_usd and hard_limit_usd:
+            fund_remain = float(hard_limit_usd) - float(soft_limit_usd)
+            logger.info(fund_remain)
+    else:
+        logger.info(response.text)
+
+
 def main():
     api_token = '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'
-    check_credit_usage(api_token)
+    get_credit_usage(api_token)
 
 
 if __name__ == '__main__':
